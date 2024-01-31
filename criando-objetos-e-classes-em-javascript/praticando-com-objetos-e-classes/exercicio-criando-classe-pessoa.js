8af2d7fbae5216816d3cfb1d1545ecba69c3cafc
Rennan Aguiar <rennan.da.silva1612@gmail.com>
2024-01-31 10:26:04 -0300

    Curso finalizado.

--- criando-objetos-e-classes-em-javascript/praticando-com-objetos-e-classes/exercicio-criando-classe-pessoa.js
+++ criando-objetos-e-classes-em-javascript/praticando-com-objetos-e-classes/exercicio-criando-classe-pessoa.js
@@ -4,6 +4,15 @@
     As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
     Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
     do seu IMC;
+
+    Elabore um algoritmo que dado o peso e a altura de um adulto mostre sua condição de acordo com a tabela abaixo.
+
+    IMC em adultos Condição:
+    - Abaixo de 18.5 Abaixo do peso;
+    - Entre 18.5 e 25 Peso normal;
+    - Entre 25 e 30 Acima do peso;
+    - Entre 30 e 40 Obeso;
+    - Acima de 40 Obsesidade Grave;
 */
 
 class Pessoa {
@@ -11,16 +20,32 @@ class Pessoa {
     peso;
     altura;
 
-    constructor (nome, peso, altura) {
+    constructor(nome, peso, altura) {
         this.nome = nome;
         this.peso = peso;
         this.altura = altura;
     }
-    apresentarImc () {
-        return `Olá meu nome é ${this.nome} e o meu imc é ${this.peso / (this.altura * this.altura)}.`;
+    apresentarImc() {
+        return this.peso / (this.altura * this.altura)
+        // `Olá meu nome é ${this.nome} e o meu imc é ${this.peso / (this.altura * this.altura)}.`;
     }
+
+    classificarImc() {
+        const imc = this.apresentarImc();
+        if (imc < 18.5) {
+            return `${this.nome} está abaixo do peso`;
+        } else if (imc >= 18.5 && imc < 25) {
+            return `${this.nome} está com o peso normal`;
+        } else if (imc >= 25 && imc < 30) {
+            return `${this.nome} está acima do peso`;
+        } else if (imc >= 30 && imc < 40) {
+            return `${this.nome} está com obesidade`;
+        } else {
+            return `${this.nome} está com obesidade grave`;
+        }
     }
+}
 
     const jose = new Pessoa('José', 70, 1.75);
 
-    console.log(jose.apresentarImc());
+    console.log(jose.classificarImc());
